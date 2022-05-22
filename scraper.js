088ddb08a387b3cea02f35687a58484376823f1f
avati123 <ashtonvati1@gmail.com>
2022-05-23 08:57:58 +1000

    Added comments at top

--- scraper.js
+++ scraper.js
@@ -2,12 +2,15 @@ const puppeteer = require('puppeteer');
 const password = ("Indie876");
 const cheerio = require('cheerio');
 const p2 = "hi";
+
+// use npm install puppeteer & cheerio to install node dependencies
+
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://login.microsoftonline.com/23a3863a-4bd3-44ea-9329-af3ee8bc6153/oauth2/v2.0/authorize?client_id=9f07b17c-c95d-4790-9aa2-c320fab6de0a&scope=openid%20email%20User.Read%20profile%20offline_access%20Directory.AccessAsUser.All%20User.Read.All&redirect_uri=https%3A%2F%2Flife-api.caulfieldlife.com.au%2FMsGraph%2Fauth%2Fopenid%2Freturn&client-request-id=b2b54bf7-2db5-4e20-9b19-53659b7cc6af&response_mode=query&response_type=code&x-client-SKU=msal.js.node&x-client-VER=1.8.0&x-client-OS=linux&x-client-CPU=x64&client_info=1');
     await page.waitForTimeout(4000);
-    await page.screenshot({ path: '<screenshots/example.png' });
+    await page.screenshot({ path: 'screenshots/example.png' });
     console.log("\nReached sign in...")
     await page.waitForSelector('#i0116');
     await page.type('#i0116', '98320@caulfieldgs.vic.edu.au');
@@ -21,7 +24,7 @@ const p2 = "hi";
     await page.click('#submitButton');
     await page.waitForTimeout(9000);
     await page.click('#idBtn_Back');
-    await page.waitForTimeout(30000);
+    await page.waitForTimeout(10000);
     console.log("\nReached CGS life...")
     await page.screenshot({path: 'screenshots/final1.png'});
 
@@ -31,16 +34,15 @@ const p2 = "hi";
 
     const pageData = await page.evaluate(() => {
         return{
-            //html: document.documentElement.innerHTML,
-
+            html: document.documentElement.innerHTML,
+            
         }
     })
 
     const $ = cheerio.load(pageData.html);
-    const element = $(".sc-gCUMDz WrlTn");
-    //console.log(pageData.html);
+    const element = $(".sc-RWGNv eYkZqg");
+    console.log(pageData)
     console.log(element.text());
-    console.log("Git tester")
 
 
     await browser.close();
